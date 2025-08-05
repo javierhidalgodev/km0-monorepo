c503ef99fa2363e3f8ad7b4b8230fe56042ed0d8
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2025-08-05 12:08:36 +0200

    ref: add following attr. to user model + refactor user service to show following && following requests from profile endpoint

--- server/src/utils/follow.service.utils.ts
+++ server/src/utils/follow.service.utils.ts
@@ -1,5 +1,5 @@
 import { FollowRequestResponseDTO } from '@/dtos/follow.dto';
-import { IUser } from '@/models/user.model';
+import { IUser, UserModel } from '@/models/user.model';
 import { AppError } from './app-error';
 
 export const requestToFollowPrivateUser = async (userToFollow: IUser, requestingUserID: string): Promise<FollowRequestResponseDTO> => {
@@ -18,12 +18,23 @@ export const requestToFollowPrivateUser = async (userToFollow: IUser, requesting
 };
 
 export const followPublicUser = async (userToFollow: IUser, requestingUserID: string): Promise<FollowRequestResponseDTO> => {
+	const requestingUser = await UserModel.findById(requestingUserID)
+
+	// Raro, porque el token no pasaría desde el middleware
+	if(!requestingUser) {
+		throw new AppError(404, 'Requesting user not found');
+	};
+
 	if (userToFollow.followers.includes(requestingUserID)) {
 		throw new AppError(400, 'Already following');
 	};
 
 	await userToFollow.updateOne({
 		$addToSet: { followers: requestingUserID },
+	});
+
+	await requestingUser.updateOne({
+		$addToSet: { following: userToFollow.id },
 	});
 
 	return {
